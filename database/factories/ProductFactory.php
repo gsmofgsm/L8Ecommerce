a48329f277ff526dac68289bb70d1e89e4253104
Qing Ma <gsmofgsm@hotmail.com>
2020-12-22 17:05:37 +0100

    Product

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = $this->getUniqueName();
+        return [
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'details' => $this->faker->sentence,
+            'price' => random_int(109999, 249999),
+            'description' => $this->faker->paragraph,
+        ];
+    }
+
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    protected function getUniqueName(): string
+    {
+        $name = $this->faker->randomElement(['Laptop', 'Iphone', 'MacBook', 'Ipad']) . ' ' . random_int(1, 9);
+        if (! Product::where('name', $name)->exists()) {
+            return $name;
+        }
+        return $this->getUniqueName();
+    }
+}
