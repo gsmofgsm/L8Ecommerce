639f4a4a35d6d8caa8a2750f8ac9542f156edf2e
Qing Ma <gsmofgsm@hotmail.com>
2020-12-23 14:31:29 +0100

    subtotal tax and total

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,7 +11,7 @@ class Product extends Model
 
     public function presentPrice()
     {
-        return money_format('$%i', $this->price / 100);
+        return presentPrice($this->price);
     }
 
     public function scopeMightAlsoLike($query)
