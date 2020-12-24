742267ed7ecb1c4b087a088bb103a63bed9f16ee
Qing Ma <gsmofgsm@hotmail.com>
2020-12-24 23:16:36 +0100

    send newTotal to Stripe

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -17,27 +17,12 @@ class CheckoutController extends Controller
      */
     public function index()
     {
-        $tax = config('cart.tax') / 100;
-        $discount = session()->get('coupon')['discount'] ?? 0;
-        $newSubtotal = Cart::subtotal() - $discount;
-        $newTax = $newSubtotal * $tax;
-        $newTotal = $newSubtotal + $newTax;
         return view('checkout')->with([
-            'discount' => $discount,
-            'newSubtotal' => $newSubtotal,
-            'newTax' => $newTax,
-            'newTotal' => $newTotal,
+            'discount' => $this->getNumbers()->get('discount'),
+            'newSubtotal' => $this->getNumbers()->get('newSubtotal'),
+            'newTax' => $this->getNumbers()->get('newTax'),
+            'newTotal' => $this->getNumbers()->get('newTotal'),
         ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
     }
 
     /**
@@ -67,16 +52,18 @@ class CheckoutController extends Controller
             $user = new User;
 
             $user->charge(
-                round(Cart::total()), $request->paymentMethod, [
+                round($this->getNumbers()->get('newTotal')), $request->paymentMethod, [
                     'metadata' => [
                         'contents' => $contents,
                         'quantity' => Cart::instance('default')->count(),
+                        'discount' => collect(session()->get('coupon'))->toJson(),
                     ]
                 ]
             );
 
             // SUCCESSFUL
             Cart::instance('default')->destroy();
+            session()->forget('coupon');
 //            return back()->with('success_message', 'Thank you! Your payment has been successfully accepted!');
             return redirect()->route('confirmation.index')->with('success_message', 'Thank you! Your payment has been successfully accepted!');
         } catch (\Exception $e) {
@@ -84,48 +71,20 @@ class CheckoutController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    private function getNumbers()
     {
-        //
-    }
+        $tax = config('cart.tax') / 100;
+        $discount = session()->get('coupon')['discount'] ?? 0;
+        $newSubtotal = Cart::subtotal() - $discount;
+        $newTax = $newSubtotal * $tax;
+        $newTotal = $newSubtotal + $newTax;
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return collect([
+            'tax' => $tax,
+            'discount' => $discount,
+            'newSubtotal' => $newSubtotal,
+            'newTax' => $newTax,
+            'newTotal' => $newTotal,
+        ]);
     }
 }
