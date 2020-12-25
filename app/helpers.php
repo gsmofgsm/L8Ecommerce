8b7f8068dbac67aea91f57b01370ca6ddf9fb154
Qing Ma <gsmofgsm@hotmail.com>
2020-12-25 11:16:26 +0100

    placeholder image

--- app/helpers.php
+++ app/helpers.php
@@ -9,3 +9,10 @@ function setActiveCategory($category, $output = 'active')
 {
     return request()->category == $category ? $output : '';
 }
+
+function productImage($image)
+{
+    return $image && file_exists('storage/'.$image)
+        ? asset('storage/'.$image)
+        : asset('storage/img/not-found.png');
+}
