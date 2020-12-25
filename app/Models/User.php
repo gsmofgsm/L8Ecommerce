e5094e2053a581c22b3b69f8147edbed8bfa3ce8
Qing Ma <gsmofgsm@hotmail.com>
2020-12-25 09:11:31 +0100

    composer install Voyager

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Cashier\Billable;
 
-class User
+class User extends Authenticatable
 {
     use HasFactory, Notifiable, Billable;
 
