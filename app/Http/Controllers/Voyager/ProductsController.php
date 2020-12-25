5c45b58ad852b393ab9ba701050dd4cc20d1a867
Qing Ma <gsmofgsm@hotmail.com>
2020-12-26 00:49:02 +0100

    show price as dollar instead of cents

--- app/Http/Controllers/Voyager/ProductsController.php
+++ app/Http/Controllers/Voyager/ProductsController.php
@@ -105,7 +105,9 @@ class ProductsController extends VoyagerBaseController
 
         // Validate fields with ajax
         $val = $this->validateBread($request->all(), $dataType->editRows, $dataType->name, $id)->validate();
-        $this->insertUpdateData($request, $slug, $dataType->editRows, $data);
+        $requestNew = $request;
+        $requestNew['price'] = $request->price * 100;
+        $this->insertUpdateData($requestNew, $slug, $dataType->editRows, $data);
 
         event(new BreadDataUpdated($dataType, $data));
 
@@ -193,7 +195,9 @@ class ProductsController extends VoyagerBaseController
 
         // Validate fields with ajax
         $val = $this->validateBread($request->all(), $dataType->addRows)->validate();
-        $data = $this->insertUpdateData($request, $slug, $dataType->addRows, new $dataType->model_name());
+        $requestNew = $request;
+        $requestNew['price'] = $request->price * 100;
+        $data = $this->insertUpdateData($requestNew, $slug, $dataType->addRows, new $dataType->model_name());
 
         event(new BreadDataAdded($dataType, $data));
 
