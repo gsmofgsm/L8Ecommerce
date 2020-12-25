1b079c72e4bbe8f57c19155fa12711821a0ea734
Qing Ma <gsmofgsm@hotmail.com>
2020-12-25 11:30:39 +0100

    multiple images for Voyager

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $casts = ['images' => 'array'];
 
     public function presentPrice()
     {
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -19,8 +19,17 @@
     </div> <!-- end breadcrumbs -->
 
     <div class="product-section container">
-        <div class="product-section-image">
-            <img src="{{ productImage($product->image) }}" alt="product">
+        <div>
+            <div class="product-section-image">
+                <img src="{{ productImage($product->image) }}" alt="product">
+            </div>
+            @if($product->images)
+            <div class="product-section-image">
+                @foreach($product->images as $image)
+                <img src="{{ productImage($image) }}" alt="product">
+                @endforeach
+            </div>
+            @endif
         </div>
         <div class="product-section-information">
             <h1 class="product-section-title">{{ $product->name }}</h1>
