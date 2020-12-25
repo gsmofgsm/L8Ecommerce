6971f3af3865276551ba17a3179ec4b87c99e730
Qing Ma <gsmofgsm@hotmail.com>
2020-12-25 14:33:52 +0100

    photo gallery

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -21,12 +21,17 @@
     <div class="product-section container">
         <div>
             <div class="product-section-image">
-                <img src="{{ productImage($product->image) }}" alt="product">
+                <img src="{{ productImage($product->image) }}" alt="product" class="active" id="currentImage">
             </div>
             @if($product->images)
-            <div class="product-section-image">
+            <div class="product-section-images">
+                <div class="product-section-thumbnail selected">
+                    <img src="{{ productImage($product->image) }}" alt="product">
+                </div>
                 @foreach($product->images as $image)
-                <img src="{{ productImage($image) }}" alt="product">
+                    <div class="product-section-thumbnail">
+                        <img src="{{ productImage($image) }}" alt="product">
+                    </div>
                 @endforeach
             </div>
             @endif
@@ -56,4 +61,30 @@
     @include('partials.might-like')
 
 
+@endsection
+
+@section('extra-js')
+    <script>
+        (function() {
+            const currentImage = document.querySelector('#currentImage');
+            const images = document.querySelectorAll('.product-section-thumbnail');
+
+            images.forEach((element) => {
+                element.addEventListener('click', thumbnailClick);
+            })
+
+            function thumbnailClick(e) {
+                // currentImage.src = this.querySelector('img').src;
+
+                currentImage.classList.remove('active');
+                currentImage.addEventListener('transitionend', () => {
+                    currentImage.src = this.querySelector('img').src;
+                    currentImage.classList.add('active');
+                })
+
+                images.forEach((element) => element.classList.remove('selected'));
+                this.classList.add('selected')
+            }
+        })();
+    </script>
 @endsection
