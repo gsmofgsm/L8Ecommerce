64584be214a087338f678362fd349881f7a99b4d
Qing Ma <gsmofgsm@hotmail.com>
2020-12-26 23:30:50 +0100

    laravel/scout with algolia engine to index Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 use Nicolaslopezj\Searchable\SearchableTrait;
 
 class Product extends Model
 {
-    use HasFactory, SearchableTrait;
+    use HasFactory, SearchableTrait, Searchable;
 
     /**
      * Searchable rules.
